f2ab77fcae33064b3d1d4af0a01b2eb6fd809a75
Andrius <andriuss0316@gmail.com>
2021-08-10 15:01:49 +0300

    20210810

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-import {} from "handlebars";
+import * as fs from "fs/promises";
+import hb from "handlebars";
+
+async function main() {
+    const html = await fs.readFile("testas.handlebars", {
+
+        const html = "<p>{{vardas}} {{pavarde}}</p>"
+
+        const template = hb.compile(html);
+
+        const zm = [
+            {
+                vardas: "Jonas",
+                pavarde: "Jonaitis"
+            },
+            {
+                vardas: "Petras",
+                pavarde: "Petraitis"
+            },
+            {
+                vardas: "Antanas",
+                pavarde: "Antanaitis"
+            }
+        ]
+
+        console.log(template({
+            zmones
+        }));
+
+    })
+}
+main();
+
+
+
 
